aa7a5ed3587999b2cc9acaa05822d62a4a89c818
Koushik Dutta <koushd@gmail.com>
2019-01-30 21:11:06 -0800

    fixup resolver

--- polyfill/fs.js
+++ polyfill/fs.js
@@ -3,7 +3,7 @@ import path from 'path';
 var files = {};
 
 function readFileSync(file) {
-    file = path.resolve(path.relative(__dirname, file));
+    file = path.resolve(file);
     var data = files[file];
     if (!data) {
         throw new Error("file not found. make sure it is packed using:\nfs.registerFile('/path/to/file', require('raw-loader!/path/to/file'))")
