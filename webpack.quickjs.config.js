2f424e19e8d98f953012c270c5ef7d7b96e42c2c
Koushik Dutta <koushd@gmail.com>
2020-12-06 17:15:30 -0800

    update

--- webpack.quickjs.config.js
+++ webpack.quickjs.config.js
@@ -100,7 +100,6 @@ module.exports = {
             mdns: path.resolve(__dirname, 'polyfill/mdns'),
             // buffer: path.resolve(__dirname, 'polyfill/buffer'),
             // buffertools: path.resolve(__dirname, 'node_modules/browserify-buffertools'),
-            'safe-buffer': path.resolve(__dirname, 'polyfill/safe-buffer'),
         },
 
         extensions: ['.tsx', '.ts', '.js']
